abeaea09dbab3322adbb90c50f002b46035d5a7e
KimMinJae <rlaalswo1105@gmail.com>
2019-12-20 22:20:01 +0900

    detail.3

--- davinci.py
+++ davinci.py
@@ -266,7 +266,8 @@ def show_p2(p2):
     print("내 타일:", end = '')
     for i in p2:
         print(i+' ', end = '')
-    
+    print()
+
 
 def more_game(): 
     a = input("새 게임을 진행 하시겠습니까? y/n: ")
